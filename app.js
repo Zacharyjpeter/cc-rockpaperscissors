f42252d7b35a7c656d4c9391db3ebd94e448cf37
Zacharyjpeter <zacharyjpeter@gmail.com>
2023-04-26 08:01:58 -0500

    fixed return statement. now output broken

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ console.log("hi");
 
 const getUserChoice = (userInput) => {
   userInput = userInput.toLowerCase();
-  if (userInput === "rock" || "paper" || "scissors");
-  return userInput;
-} else {
-  console.log("Error. Invalid entry");
+  if (userInput === "rock" || "paper" || "scissors") {
+    return userInput;
+  } else {
+    console.log("Error");
+  }
 };
+
+//output test
+console.log(getUserChoice("plane"));
